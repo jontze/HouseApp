8b85568122b016c3acaa562e5c8b3cfdd4aa3495
UsingPython <jonnyschultze@hotmail.de>
2021-01-05 18:53:16 +0100

    export SkeletonComponent

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -3,24 +3,23 @@ import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
-import { NavComponent } from './components/nav/nav.component';
-import { HomeComponent } from './components/home/home.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SkeletonComponent } from './components/skeleton/skeleton.component';
 
 const components: any[] = [
-  NavComponent,
   NavigationBarComponent,
-  HomeComponent
+  SkeletonComponent
 ];
 
 @NgModule({
-  declarations: [ NavComponent, NavigationBarComponent ],
+  declarations: [ NavigationBarComponent, FooterComponent, SkeletonComponent ],
   imports: [
     AngularMaterialModule,
     BrowserAnimationsModule,
     CommonModule,
     RouterModule
   ],
-  exports: [ NavComponent ]
+  exports: [ SkeletonComponent ]
 })
 export class CoreModule { }
