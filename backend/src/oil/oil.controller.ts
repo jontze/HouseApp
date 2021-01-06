943b1d1f36b2480af810542f09da0bf0c67738a2
UsingPython <jonnyschultze@hotmail.de>
2021-01-06 02:00:10 +0100

    create oil endpoint

--- backend/src/oil/oil.controller.ts
+++ backend/src/oil/oil.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { OilService } from './oil.service';
 
 @Controller('oil')
-export class OilController {}
+export class OilController {
+  constructor(private readonly oilService: OilService) {}
+
+  @Get()
+  async allOil() {
+    return await this.oilService.oils({
+      where: {
+        id: {
+          gt: 0,
+        },
+      },
+    });
+  }
+
+  @Post()
+  async createOil() {
+    // save params in Oil-Table
+  }
+}
