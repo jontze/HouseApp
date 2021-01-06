e3cef608cd447d94c32414c429221fd432f4cec8
UsingPython <jonnyschultze@hotmail.de>
2021-01-06 01:47:06 +0100

    add oil prisma services

--- backend/src/oil/oil.service.ts
+++ backend/src/oil/oil.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Oil, Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class OilService {}
+export class OilService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async oil(
+    oilWhereUniqueInput: Prisma.OilWhereUniqueInput,
+  ): Promise<Oil | null> {
+    return this.prisma.oil.findUnique({
+      where: oilWhereUniqueInput,
+    });
+  }
+
+  async oils(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.OilWhereUniqueInput;
+    where: Prisma.OilWhereInput;
+    orderBy?: Prisma.OilOrderByInput;
+  }): Promise<Oil[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.oil.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async countOil(params: Prisma.FindManyOilArgs): Promise<number> {
+    return await this.prisma.oil.count(params);
+  }
+
+  async updateOil(params: {
+    where: Prisma.OilWhereUniqueInput;
+    data: Prisma.OilUpdateInput;
+  }): Promise<Oil> {
+    const { where, data } = params;
+    return this.prisma.oil.update({
+      data,
+      where,
+    });
+  }
+
+  async createOil(data: Prisma.OilCreateInput): Promise<Oil> {
+    const newOil = await this.createOil(data);
+    if (!newOil) {
+      throw new HttpException('Oil foundation failed.', HttpStatus.FORBIDDEN);
+    } else {
+      return newOil;
+    }
+  }
+}
