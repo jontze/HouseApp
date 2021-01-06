b42bf41684ed03895e4d3d72b2a2ca73cc4ba252
UsingPython <jonnyschultze@hotmail.de>
2021-01-06 01:50:21 +0100

    add Power Prisma Services

--- backend/src/power/power.service.ts
+++ backend/src/power/power.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Power, Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class PowerService {}
+export class PowerService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async power(
+    powerWhereUniqueInput: Prisma.PowerWhereUniqueInput,
+  ): Promise<Power | null> {
+    return this.prisma.power.findUnique({
+      where: powerWhereUniqueInput,
+    });
+  }
+
+  async powers(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.PowerWhereUniqueInput;
+    where: Prisma.PowerWhereInput;
+    orderBy?: Prisma.PowerOrderByInput;
+  }): Promise<Power[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.power.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async countPower(params: Prisma.FindManyPowerArgs): Promise<number> {
+    return await this.prisma.power.count(params);
+  }
+
+  async updatePower(params: {
+    where: Prisma.PowerWhereUniqueInput;
+    data: Prisma.PowerUpdateInput;
+  }): Promise<Power> {
+    const { where, data } = params;
+    return this.prisma.power.update({
+      data,
+      where,
+    });
+  }
+
+  async createPower(data: Prisma.PowerCreateInput): Promise<Power> {
+    const newPower = await this.createPower(data);
+    if (!newPower) {
+      throw new HttpException('Power foundation failed.', HttpStatus.FORBIDDEN);
+    } else {
+      return newPower;
+    }
+  }
+}
