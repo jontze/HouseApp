35ed482585c715ba570f91b1b1af1bac2004fdaf
UsingPython <jonnyschultze@hotmail.de>
2021-01-06 02:05:44 +0100

    create water endpoint

--- backend/src/water/water.controller.ts
+++ backend/src/water/water.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { WaterService } from './water.service';
 
 @Controller('water')
-export class WaterController {}
+export class WaterController {
+  constructor(private readonly waterService: WaterService) {}
+
+  @Get()
+  async allWater() {
+    return this.waterService.waters({
+      where: {
+        id: {
+          gt: 0,
+        },
+      },
+    });
+  }
+
+  @Post()
+  async createWater() {
+    // save water to taböle
+  }
+}
