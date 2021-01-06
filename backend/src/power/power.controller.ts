1e0ad516cfb1d983e104daf0225db0cc079cc110
UsingPython <jonnyschultze@hotmail.de>
2021-01-06 02:05:36 +0100

    create power endpoint

--- backend/src/power/power.controller.ts
+++ backend/src/power/power.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { PowerService } from './power.service';
 
 @Controller('power')
-export class PowerController {}
+export class PowerController {
+  constructor(private readonly powerService: PowerService) {}
+
+  @Get()
+  async allPower() {
+    return await this.powerService.powers({
+      where: {
+        id: {
+          gt: 0,
+        },
+      },
+    });
+  }
+
+  @Post()
+  async createPower() {
+    // save power to table
+  }
+}
