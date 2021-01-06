3c4aca91c2f7fb4da29534b292cf0afb99cc0a26
UsingPython <jonnyschultze@hotmail.de>
2021-01-06 01:30:43 +0100

    add module import

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { PowerModule } from './power/power.module';
+import { WaterModule } from './water/water.module';
+import { OilModule } from './oil/oil.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, PowerModule, WaterModule, OilModule],
   controllers: [AppController],
   providers: [AppService],
 })
