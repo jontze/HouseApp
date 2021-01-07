f730c9d72d878b83a667209c06dc5467910ca7b2
UsingPython <jonnyschultze@hotmail.de>
2021-01-07 20:58:48 +0100

    init chart component

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
+import { ChartComponent } from './chart/chart.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ChartComponent],
   imports: [
     AngularMaterialModule,
     HomeRoutingModule
