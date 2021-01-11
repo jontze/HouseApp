42eaf803502a2bb8ea056ed537bfe0e76f92770d
UsingPython <jonnyschultze@hotmail.de>
2021-01-11 21:07:33 +0100

    enableCors to *

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,6 +9,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('/api');
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   // Set up Helmet
   app.use(helmet());
   // Set up rate limiting
