2a632369d3a4e90f6f316187772eb2be843c6387
UsingPython <jonnyschultze@hotmail.de>
2021-01-11 15:10:46 +0100

    add Snackbar on submited data

--- frontend/src/app/creator/creator.component.ts
+++ frontend/src/app/creator/creator.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ApiBackendService } from '../core/services/api-backend.service';
 import { Oil, Power, Water } from '../core/services/classes/api-backend';
 
@@ -24,32 +25,51 @@ export class CreatorComponent implements OnInit {
   private postedOil!: Oil;
   private postedWater!: Water;
   private postedPower!: Power;
+  private readonly snackDuration: number = 7000;
+  private readonly snackAction: string = 'Ok';
 
-  constructor(private readonly fb: FormBuilder, private readonly apiBackendService: ApiBackendService) { }
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly apiBackendService: ApiBackendService,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
   }
 
-  submitPower(): void {
+  public submitPower(): void {
     this.apiBackendService.postPower({
       date: this.powerForm.value.date,
       kwh: parseFloat(this.powerForm.value.kwh),
-    }).subscribe((res) => this.postedPower = res);
+    }).subscribe((res) => {
+      this.postedPower = res;
+      this.showSnack('Stromzähler-Stand gespeichert!', this.snackAction, this.snackDuration);
+    });
   }
 
-  submitWater(): void {
+  public submitWater(): void {
     this.apiBackendService.postWater({
       date: this.waterForm.value.date,
       cubicmeter: parseFloat(this.waterForm.value.m3),
-    }).subscribe((res) => this.postedWater = res);
+    }).subscribe((res) => {
+      this.postedWater = res;
+      this.showSnack('Wasserzähler-Stand gespeichert!', this.snackAction, this.snackDuration);
+    });
   }
 
-  submitOil(): void {
+  public submitOil(): void {
     this.apiBackendService.postOil({
       date: this.oilForm.value.date,
       filled: parseFloat(this.oilForm.value.filled)
     }).subscribe((res) => {
       this.postedOil = res;
+      this.showSnack('Ölstand gespeichert!', this.snackAction, this.snackDuration);
+    });
+  }
+
+  public showSnack(message: string, action: string, showTime: number): void {
+    this.snackBar.open(message, action, {
+      duration: showTime,
     });
   }
 
