801bade47c5afec44b91957405f8727325ec832f
UsingPython <jonnyschultze@hotmail.de>
2021-01-11 11:52:37 +0100

    create Settings for each Chart

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiBackendService } from '../core/services/api-backend.service';
 import { Oil, Power, Water } from '../core/services/classes/api-backend';
+import { ChartSettingsI } from './chart/classes/chart';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,30 @@ import { Oil, Power, Water } from '../core/services/classes/api-backend';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  public allOil: Oil[] = [];
-  public allPower: Power[] = [];
-  public allWater: Water[] = [];
+  public allOil!: Oil[];
+  public allPower!: Power[];
+  public allWater!: Water[];
+  public readonly oilChartSettings: ChartSettingsI[] = [
+    {
+      dateFieldName: 'date',
+      valueFieldName: 'filled',
+      label: 'Ölstand',
+    }
+  ];
+  public readonly powerChartSettings: ChartSettingsI[] = [
+    {
+      dateFieldName: 'date',
+      valueFieldName: 'kwh',
+      label: 'Stromverbrauch (KWh)'
+    }
+  ];
+  public readonly waterChartSettings: ChartSettingsI[] = [
+    {
+      dateFieldName: 'date',
+      valueFieldName: 'cubicmeter',
+      label: 'Wasserverbrauch (m³)'
+    }
+  ];
 
   constructor(private apiBackendService: ApiBackendService) { }
 
