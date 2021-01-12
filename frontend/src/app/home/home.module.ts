b1c4e35ae1e91bccbe660d38256569213a4aa7a8
UsingPython <jonnyschultze@hotmail.de>
2021-01-12 11:40:23 +0100

    implement compare-component

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -3,11 +3,12 @@ import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { ChartComponent } from './chart/chart.component';
+import { CompareComponent } from './compare/compare.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, ChartComponent],
+  declarations: [HomeComponent, ChartComponent, CompareComponent],
   imports: [
     AngularMaterialModule,
     HomeRoutingModule
