f41e4f3b86bf6166f86ae0ac561d591769cedff3
UsingPython <jonnyschultze@hotmail.de>
2021-01-13 15:56:37 +0100

    remove prismaModule import, add TypeORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PrismaModule } from './prisma/prisma.module';
 import { PowerModule } from './power/power.module';
 import { WaterModule } from './water/water.module';
 import { OilModule } from './oil/oil.module';
 
 @Module({
-  imports: [
-    PrismaModule,
-    PowerModule,
-    WaterModule,
-    OilModule,
-    TypeOrmModule.forRoot(),
-  ],
+  imports: [PowerModule, WaterModule, OilModule, TypeOrmModule.forRoot()],
 })
 export class AppModule {}
