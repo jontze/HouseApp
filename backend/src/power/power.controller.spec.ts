14e8ffc8997e3ea331ce466c1926ed2ae05db60d
UsingPython <jonnyschultze@hotmail.de>
2021-01-13 22:00:27 +0100

    improve controller tests

--- backend/src/power/power.controller.spec.ts
+++ backend/src/power/power.controller.spec.ts
@@ -1,14 +1,18 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Power } from 'src/entity/power.entity';
 import { Repository } from 'typeorm';
 import { PowerController } from './power.controller';
 import { PowerService } from './power.service';
+import * as request from 'supertest';
 
 describe('PowerController', () => {
+  let app: INestApplication;
   let controller: PowerController;
   let service: PowerService;
   let repo: Repository<Power>;
+  let testPower: Power;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -25,6 +29,21 @@ describe('PowerController', () => {
     controller = module.get<PowerController>(PowerController);
     service = module.get<PowerService>(PowerService);
     repo = module.get<Repository<Power>>(getRepositoryToken(Power));
+    testPower = {
+      id: 1,
+      date: new Date(),
+      kwh: 120,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    app = module.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 
   it('should be defined', () => {
@@ -37,5 +56,19 @@ describe('PowerController', () => {
 
   it('service should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return all Power on GET request', () => {
+    jest.spyOn(service, 'powers').mockResolvedValueOnce([testPower]);
+    request(app.getHttpServer()).get('/power').expect(200).expect([testPower]);
+  });
+
+  it('should return the posted Power on POST request', () => {
+    jest.spyOn(service, 'create').mockResolvedValueOnce(testPower);
+    request(app.getHttpServer())
+      .post('/power')
+      .send(testPower)
+      .expect(201)
+      .expect(testPower);
   });
 });
