61cee1bc6af03fc9c303bb408fcf875217df175c
UsingPython <jonnyschultze@hotmail.de>
2021-01-13 12:56:50 +0100

    add TypeORM to import

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PrismaModule } from './prisma/prisma.module';
 import { PowerModule } from './power/power.module';
 import { WaterModule } from './water/water.module';
 import { OilModule } from './oil/oil.module';
 
 @Module({
-  imports: [PrismaModule, PowerModule, WaterModule, OilModule],
+  imports: [
+    PrismaModule,
+    PowerModule,
+    WaterModule,
+    OilModule,
+    TypeOrmModule.forRoot(),
+  ],
 })
 export class AppModule {}
