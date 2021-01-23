98afe31f489dc70ad6dcf2ac25c6f4bb1af40991
UsingPython <42588836+UsingPython@users.noreply.github.com>
2021-01-23 20:14:15 +0100

    remove materials from shared module

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AngularMaterialModule } from '../angular-material/angular-material.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, AngularMaterialModule],
-  exports: [CommonModule, AngularMaterialModule],
+  imports: [CommonModule],
+  exports: [CommonModule],
 })
 export class SharedModule {}
